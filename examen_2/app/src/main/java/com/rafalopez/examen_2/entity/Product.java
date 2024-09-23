6c14e3d409f5414c5aafba1bd67abcada1deffbf
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-09-23 18:57:07 -0300

    inciando

---
+++ examen_2/app/src/main/java/com/rafalopez/examen_2/entity/Product.java
@@ -0,0 +1,57 @@
+package com.rafalopez.examen_2.entity;
+
+public class Product {
+    private String code;
+    private String desciption;
+    private int stock;
+    private double price;
+
+    public Product(String code, String desciption, int stock, double price) {
+        this.code = code;
+        this.desciption = desciption;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "code='" + code + '\'' +
+                ", desciption='" + desciption + '\'' +
+                ", stock=" + stock +
+                ", price=" + price +
+                '}';
+    }
+}
