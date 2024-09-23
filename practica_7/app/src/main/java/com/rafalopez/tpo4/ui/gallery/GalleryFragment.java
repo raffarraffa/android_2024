d6539d99b59f48747734ebf259a27e413a49012f
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-09-22 23:43:28 -0300

    Update GalleryFragment.java

--- practica_7/app/src/main/java/com/rafalopez/tpo4/ui/gallery/GalleryFragment.java
+++ practica_7/app/src/main/java/com/rafalopez/tpo4/ui/gallery/GalleryFragment.java
@@ -18,17 +18,19 @@ import com.rafalopez.tpo4.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends FragmentActivity {
     private GalleryViewModel mv;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mv =  ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
-        mv.getMapaActual().observe(this, new Observer<mv.MapaActual>() {
+        mv =
+                ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(GalleryViewModel.class);
+        mv.getMapaActual().observe(this, new Observer<GalleryViewModel.MapaActual>() {
             @Override
-            public void onChanged(mvtivityViewModel.MapaActual mapaActual) {
+            public void onChanged(GalleryViewModel.MapaActual mapaActual) {
                 ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(mapaActual);
             }
         });
-        mv.obtenerMapa(); // opcion llamr map desde main activity
+        mv.obtenerMapa();
     }
 
 
