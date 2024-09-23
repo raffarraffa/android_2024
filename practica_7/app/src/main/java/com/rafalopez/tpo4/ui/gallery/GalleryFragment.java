94bc76e1dac87143a63cf745505f53107df0fe97
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-09-22 23:56:29 -0300

    no aanda

--- practica_7/app/src/main/java/com/rafalopez/tpo4/ui/gallery/GalleryFragment.java
+++ practica_7/app/src/main/java/com/rafalopez/tpo4/ui/gallery/GalleryFragment.java
@@ -1,20 +1,13 @@
 package com.rafalopez.tpo4.ui.gallery;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.rafalopez.tpo4.R;
-import com.rafalopez.tpo4.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends FragmentActivity {
     private GalleryViewModel mv;
